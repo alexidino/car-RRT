0b7fe17ed34d9626c8192196807a2e04b0bab84b
alexidino <95204103+alexidino@users.noreply.github.com>
2022-08-31 12:34:38 +0300

    add JS

---
+++ common/js/style.js
@@ -0,0 +1,11 @@
+// function toggleScrollClass() {
+//   const header = document.querySelectorAll('.advantage-main')[0];
+//   window.pageYOffset > 0 ?  header.classList.add('fade') : header.classList.remove('fade');
+// }
+// window.addEventListener('scroll', function() {toggleScrollClass()});
+// toggleScrollClass();
+// А затем просто создайте класс.fade в своем CSS, чтобы сделать что-то другое с фоном:
+
+// .navbar-default.fade {
+//   background-color: transparent !important;
+// }
